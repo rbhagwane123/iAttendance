09646fb84ff0e441f7b249943dbb8f8a31bab7fb
Hitesh <hjoshi30042001@gmail.com>
2024-03-21 22:39:01 +0530

    commit from rbhagwane123/iattendace

--- app/src/main/java/com/example/iattendance/MainActivity.java
+++ app/src/main/java/com/example/iattendance/MainActivity.java
@@ -29,5 +29,6 @@ public class MainActivity extends AppCompatActivity {
 
         //New message Added from rbhagwane123@gmail.com
         //New message added Try to fetch it
+        //Add new data to Github from Hitesh
     }
 }
