9429a1ec3c8363fe6618bde2c6b31e94f82d257f
Hitesh <hjoshi30042001@gmail.com>
2024-03-24 17:38:46 +0530

    commit from Hitesh

--- app/src/main/java/com/example/iattendance/Faculty_Attendance_Screen/FacultyAttendancePg.java
+++ app/src/main/java/com/example/iattendance/Faculty_Attendance_Screen/FacultyAttendancePg.java
@@ -1,16 +1,23 @@
 package com.example.iattendance.Faculty_Attendance_Screen;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 import com.example.iattendance.R;
+import com.google.android.material.tabs.TabLayout;
 
 public class FacultyAttendancePg extends AppCompatActivity {
+    TabLayout stud_tab;
+    ViewPager stud_viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.faculty_attendance_pg);
+
+        stud_tab = findViewById(R.id.stud_tab);
+        stud_viewPager = findViewById(R.id.stud_viewPager);
     }
 }
